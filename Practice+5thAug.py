0e52271222d2cb7e1aed47b7b325cec02b14b8c5
Person_1 <maryhennessy@netscape.net>
2016-08-05 16:51:51 +0100

    Add files via upload

---
+++ Practice+5thAug.py
@@ -0,0 +1,41 @@
+
+# coding: utf-8
+
+# In[2]:
+
+lst = [1,2,3,4,5]
+for i in lst:
+    print 'yes'
+
+
+# In[12]:
+
+lst =[ i for i in range (1,6,2)]
+print lst
+
+
+# In[14]:
+
+list1=[i**2 for i in range (1,10,3)]
+print list1
+
+
+# In[19]:
+
+input1= raw_input('enter name: ')
+if input1 =='mary':
+    print 'okay'
+else:
+    print 'who'
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
